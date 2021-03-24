9db739023cd76c1b9bc036d2174d0b7f5efcb6a2
thuymieu <77791500+thuymieu@users.noreply.github.com>
2021-03-24 08:41:55 +0700

    Update Program.cs

--- CuaHangDoChoi/Program.cs
+++ CuaHangDoChoi/Program.cs
@@ -18,7 +18,7 @@ namespace CuaHangDoChoi
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Fixed main
+            //fixed main
         }
     }
 }
